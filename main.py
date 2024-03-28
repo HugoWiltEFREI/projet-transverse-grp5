7de644c50b323bf4bd564b39647d2a96a628b8ac
HugoWiltEFREI <hugo.wilt@efrei.net>
2024-03-28 10:58:00 +0100

    heheheha

--- main.py
+++ main.py
@@ -1,106 +1,186 @@
-import pygame.mixer
+import pygame
 from pygame.locals import *
 
-from functions import *
-from menu import menu
-
 pygame.init()
+
 clock = pygame.time.Clock()
 
 pygame.display.set_caption('Game')
 game_font = pygame.font.Font("VT323-Regular.ttf", int(100))
 text = game_font.render("PRESS E", False, "brown")
 zone_de_text = ""
+affichage = 1
+
+WINDOW_SIZE = (0, 0)
+# This will be the Surface where we will blit everything
+display = pygame.Surface((1920, 1080))
+# Then we will scale (every frame) the display onto the screen
+screen = pygame.display.set_mode(WINDOW_SIZE, pygame.FULLSCREEN)
 
 moving_right = False
 moving_left = False
 # For the pygame.transform.flip(player_img, 1, 0)
-
 stay_right = True
 momentum = 0
 air_timer = 0
 
-liste_music = ["musics/Rick Astley - Never Gonna Give You Up (Official Music Video).wav", "musics/Undertale_Chill.wav",
-               "musics/Rick Astley - Never Gonna Give You Up (Official Music Video).wav", "musics/Undertale_Chill.wav"]
+grassimage = pygame.image.load("grassMid.png")
+grasscenter = pygame.image.load("grassCenter.png")
 
-grassimage = pygame.image.load("textures/grassMid.png")
-grasscenter = pygame.image.load("textures/grassCenter.png")
-bow = pygame.image.load("textures/bow.png")
-bluegrass = pygame.image.load("textures/grassCenterBlue.png")
-bluegrassMid = pygame.image.load("textures/grassMiddleBlue.png")
-darkBlock = pygame.image.load("textures/texture mario underground.png")
+scroll = [0, 0]
 
-tl = {"o": grassimage, "x": grasscenter, "l": bluegrass, "b": bluegrassMid, 'd': darkBlock}
-game_font2 = pygame.font.Font("VT323-Regular.ttf", int(150))
-text2 = game_font2.render("PRESS R TO RESTART", False, "brown")
+game_map2 = """
+------------------------------
+------------------------------
+o------ooo--------o-----------
+x---oooxx--------ox-----------
+x------xxoooo-----x-----------
+xooo---xx-------oox-----------
+x-----------------x-----------
+x---ooooooooooooo-x-----------
+x------xx---------x-----------
+xooooooxx-oooooooox-----------
+xxxxxxxxx---------------------
+x-----------------------------
+x-----------------------------
+xooooooooooooooooooooooooooooo
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+""".splitlines()
 
-player_img = pygame.image.load('textures/perso.png')
+game_map1 = """
+-------------------------------------------------------------------------------------------------0------------------
+-------------------------------------------------------------------------------------------------0------------------
+-------------------------------------------------------------------------------------------------0------------------
+-------------------------------------------------------------------------------------------------0------------------
+-------------------------------------------------------------------------------------------------0------------------
+ooo----------------------------------------------------------------------------------------------0------------------
+xxxo------------------oooooo---------------------------------------------------------------------0------------------
+xxxxooooooooooooooo----------o-----------------------------------------oooooooo-----------------0------------------
+xxxxxxxxxxxxxxxxxxxo----------x----------o---------o------------oooo-------------------oooo------0------------------
+xxxxxxxxxxxxxxxxxxxxooooooooooxoooooooooox--oooooooxoooooooooo-----------------ooo------------ooo0------------------
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx--xxxxxxxxxxxxxxxxxx--------------------------------xxx0------------------
+""".splitlines()
+
+game_map = [list(lst) for lst in game_map1]
+bow = pygame.image.load("bow.png")
+spike = pygame.image.load("spike.png")
+
+tl = {"o": grassimage, "x": grasscenter}
+
+player_img = pygame.image.load('perso.png')
 player_img = pygame.transform.scale_by(player_img, 0.04)
 player_img.set_colorkey((255, 255, 255))
 
+player_rect = pygame.Rect(25, 25, 30, 40)
+
+nombre_de_vie = 3
 now = 0
-level = 1
+level = 2
+display_dead = 0
 derniereaction = 0
 
+loop = 1
+player_velocity_multi = 1
 
-def event_manager():
-    global is_running, x, y
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            is_running = False
 
-        if (event.type == KEYDOWN) and (statut == 1) and (model.affichage != 0):
-            if event.key == K_e:
-                model.zone_de_text = "Bow acquired"
-                model.affichage = 0
+coeur = pygame.image.load("hud_heartFull.png")
+game_font2 = pygame.font.Font("VT323-Regular.ttf", int(150))
+text2 = game_font2.render("PRESS R TO RESTART", False, "brown")
 
-        if pygame.mouse.get_pressed()[0]:
-            x, y = pygame.mouse.get_pos()
-            print(x, y)
 
-        is_dead(event)
+def collision_test(rect, tiles):
+    #Return le rect en collision avec le player
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
 
+def move(rect, movement, tiles):
+    collision_types = {
+        'top': False, 'bottom': False, 'right': False, 'left': False}
+    rect.x += movement[0]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collision_types['right'] = True
+        elif movement[0] < 0:
+            rect.left = tile.right
+            collision_types['left'] = True
+    rect.y += movement[1]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types['bottom'] = True
+        elif movement[1] < 0:
+            rect.top = tile.bottom
+            collision_types['top'] = True
+    return rect, collision_types
+
+
+def isinzone(x1, x2, x3, y1, y2, y3):
+    if (x1 < x2 < x3) and (y1 < y2 < y3):
+        return 1
+    else:
+        return 0
+
+
+def life_left(nombre_de_vie):
+    i = 0
+    while i < nombre_de_vie:
+        screen.blit(coeur, (1500 + i * 80, 50))
+        i += 1
+
+def diedFromVoid(posY):
+    global nombre_de_vie
+    if (posY>1000):
+        nombre_de_vie = 0
+
+
+def spike_level(level):
+    if level == 1:
+        display.blit(spike, (1085 - scroll[0], 850 - scroll[1]))
+        display.blit(spike, (750 - scroll[0], 850 - scroll[1]))
+    if level == 2:
+        display.blit(spike, (985 - scroll[0], 465 - scroll[1]))
+        display.blit(spike, (750 - scroll[0], 465 - scroll[1]))
+
+
+def is_dead():
+    global display_dead, nombre_de_vie, player_velocity_multi
+    if nombre_de_vie <= 0:
+        display_dead = 1
+        player_velocity_multi = 0
         if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                is_running = False
-            if event.key == K_c:
-                print(player_rect.x, player_rect.y)
-            if event.key == K_n:
-                if model.level >= 3:
-                    model.level = 0
-                else:
-                    model.level += 1
-                pygame.mixer.music.load(liste_music[model.level - 1])
-                pygame.mixer.music.play()
-            if event.key == K_RIGHT or event.key == K_d:
-                model.moving_right = True
-                model.stay_right = True
-            if event.key == K_LEFT or event.key == K_q:
-                model.moving_left = True
-                model.stay_right = False
-            if event.key == K_SPACE or event.key == K_UP or event.key == K_z:
-                if 0 in model.falling:
-                    model.momentum = 10
-                    model.falling.pop(0)
-                    model.falling.append(1)
-        if event.type == KEYUP:
-            if event.key == K_RIGHT or event.key == K_d:
-                model.moving_right = False
-            if event.key == K_LEFT or event.key == K_q:
-                model.moving_left = False
+            if event.key == K_r:
+                nombre_de_vie = 3
+                display_dead = 0
+                player_rect.x = 10
+                player_rect.y = 10
+                player_velocity_multi = 1
+                scroll[0], scroll[1] = 0, 0
+
+def level_actions(level):
+    spike_level(level)
 
 
-def unnamed():
-    global y, x, player_rect, statut
+while loop:
+    # CLEAR THE SCREEN
     display.fill((146, 244, 255))
+
     if player_rect.x > 950:
-        scroll[0] = player_rect.x - 950
+        scroll[0] += int(player_rect.x - scroll[0] - 950)
     if player_rect.y < 150:
-        scroll[1] = player_rect.y - 540
+        scroll[1] += (player_rect.y - scroll[1] - 540)
+
     # Affichage des blocks
     tile_rects = []
     y = 0
-    for line_of_symbols in select_map(model.level):
+    for line_of_symbols in game_map:
         x = 0
         for symbol in line_of_symbols:
             if symbol in tl:
@@ -112,76 +192,98 @@ def unnamed():
                 tile_rects.append(pygame.Rect(x * 64, y * 64, 64, 64))
             x += 1
         y += 1
-    spike_level(model.level)
+
+    spike_level(level)
+
     # MOVEMENT OF THE PLAYER
     player_movement = [0, 0]
-    if model.moving_right:
-        player_movement[0] += 6 * model.player_velocity_multi
-    if model.moving_left:
-        player_movement[0] -= 6 * model.player_velocity_multi
-    player_movement[1] -= model.momentum
-    if model.falling[-1]:
-        model.momentum -= 0.3 * model.player_velocity_multi
+    if moving_right:
+        player_movement[0] += 6 * player_velocity_multi
+    if moving_left:
+        player_movement[0] -= 6 * player_velocity_multi
+    player_movement[1] += momentum
+    momentum += 0.3 * player_velocity_multi
+    if momentum > 3:
+        momentum = 3
+
     player_rect, collisions = move(player_rect, player_movement, tile_rects)
+
     if collisions['bottom']:
-        model.momentum = 0
-        model.falling.append(0)
-        model.falling.pop(0)
+        air_timer = 0
+        momentum = 0
     else:
-        model.falling.append(1)
-        model.falling.pop(0)
+        air_timer += 1
     if collisions["top"]:
-        model.momentum *= -1
+        momentum = 0
+
     # Flip the player image when goes to the left
-    if model.player_velocity_multi == 1:
-        if model.stay_right:
+    if player_velocity_multi == 1:
+        if stay_right:
             display.blit(
                 player_img, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
         else:
             display.blit(
-                pygame.transform.flip(player_img, True, False),
+                pygame.transform.flip(player_img, 1, 0),
                 (player_rect.x - scroll[0], player_rect.y - scroll[1]))
+
     statut = isinzone(450, player_rect.x, 515, 455, player_rect.y, 515)
-    if statut and model.affichage == 1:
+    if statut and affichage == 1:
         display.blit(text, (800, 875))
 
-    if model.level == 1:
-        isinspike1 = isinzone(725, player_rect.x, 790, 490, player_rect.y + 30, 510)
-        isinspike2 = isinzone(960, player_rect.x, 1025, 490, player_rect.y + 30, 510)
-    elif model.level == 2:
-        isinspike1 = isinzone(850 + 200, player_rect.x, 915 + 200, 490, player_rect.y + 30, 510)
-        isinspike2 = isinzone(1085 + 200, player_rect.x, 1150 + 200, 490, player_rect.y + 30, 510)
-    else:
-        isinspike1 = False
-        isinspike2 = False
+    isinspike1 = isinzone(725, player_rect.x, 790, 490, player_rect.y + 30, 510)
+    isinspike2 = isinzone(960, player_rect.x, 1025, 490, player_rect.y + 30, 510)
 
     if isinspike1 or isinspike2:
-        if model.now - model.derniereaction > 2000:
-            model.number_of_life -= 1
-            model.derniereaction = model.now
+        if now - derniereaction > 2000:
+            nombre_de_vie -= 1
+            derniereaction = now
 
-    model.now = pygame.time.get_ticks()
-    cpteur = game_font.render(str(model.zone_de_text), False, "brown")
-    diedFromVoid(player_rect.y)
-    event_manager()
+    now = pygame.time.get_ticks()
+
+    cpteur = game_font.render(str(zone_de_text), False, "brown")
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            loop = 0
+
+        if (event.type == KEYDOWN) and (statut == 1) and (affichage != 0):
+            if event.key == K_e:
+                zone_de_text = "Bow acquired"
+                affichage = 0
+
+        if pygame.mouse.get_pressed()[0]:
+            x, y = pygame.mouse.get_pos()
+            print(x, y)
+
+        is_dead()
+        diedFromVoid(player_rect.y)
+
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                loop = 0
+            if event.key == K_RIGHT:
+                moving_right = True
+                stay_right = True
+            if event.key == K_LEFT:
+                moving_left = True
+                stay_right = False
+            if event.key == K_SPACE or event.key == K_UP:
+                if air_timer < 3:
+                    momentum = -9
+        if event.type == KEYUP:
+            if event.key == K_RIGHT:
+                moving_right = False
+            if event.key == K_LEFT:
+                moving_left = False
+
     screen.blit(pygame.transform.scale(display, (1920, 1080)), (0, 0))
     screen.blit(cpteur, (30, 30))
-    if model.affichage != 0:
+    if affichage != 0:
         screen.blit(bow, (465 - scroll[0], 465 - scroll[1]))
-    if model.display_dead != 0:
+    if display_dead != 0:
         screen.blit(text2, (400, 100))
-    life_left()
+    life_left(nombre_de_vie)
     pygame.display.update()
     clock.tick(60)
 
-
-pygame.mixer.music.load(liste_music[model.level - 1])
-pygame.mixer.music.play()
-
-is_running = True
-
-while is_running:
-    if model.show_menu == 1:
-        menu()
-    else:
-        unnamed()
+pygame.quit()
